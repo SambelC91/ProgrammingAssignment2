4a44a9105852ac68e196e3f967d9551efca4df9e
SambelC91 <112668700+SambelC91@users.noreply.github.com>
2022-09-02 13:01:23 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,20 +4,34 @@
 ## Write a short comment describing this function
 
 ##make cache matrix
-
+##  Creates a special matrix object that can cache its inverse
 makecachematrix <- function (x = matrix())
-inv <- NULL
+
+  ##Initialize the inverse property
+  inv <- NULL
+
+## Method to set the matrix
 set <- function (y)
   x <<- y
   inv <<- NULL
 
+##  Method the get the matrix
 get <- function() x 
+
+## Method to set the inverse of the matrix
 setinverse <- function() inv <<- solve (x)
+
+
+## Method to get the inverse of the matrix
 getinverse <- function() inv
+
+## Return a list of said methods
 list(set = set,
      get = get,
      setinverse = setinverse,
      getinverst = getinverse)
+
+
 
 funs <- makeCacheMatrix()
 funs$set(matrix(1:4, 2))
